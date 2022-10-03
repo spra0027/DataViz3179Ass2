d2fdb902ac5368ec7a5ad1475af7f719e3efab42
Shakthi <spra0027@student.monash.edu>
2022-10-03 18:57:24 +1100

    removed empty countries

--- js/assignment_2_empty.js
+++
@@ -1,4 +0,0 @@
-var spec2 = "assignment_2_empty.json";
-vegaEmbed('#choropleth_map_empty', spec2).then(function(result) {
-  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
