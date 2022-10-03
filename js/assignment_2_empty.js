77284797e83aba1c35823f668e04af864d4ef608
Shakthi <spra0027@student.monash.edu>
2022-10-03 18:17:15 +1100

    added html for empty countries

---
+++ js/assignment_2_empty.js
@@ -0,0 +1,4 @@
+var spec2 = "assignment_2_empty.json";
+vegaEmbed('#choropleth_map_empty', spec2).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
